1b5542942ba2c2889af88781dc82415a88bd77b3
Animesh Mathur <animesh0107@gmail.com>
2026-04-13 15:57:56 -0500

    Removed prod from url

--- lambda/packages/accounts_lambda_package/src/app.py
+++ lambda/packages/accounts_lambda_package/src/app.py
@@ -5,7 +5,7 @@ from src.routers import accounts
 app = FastAPI(
     title="Accounts API",
     version="0.1.0",
-    root_path="/prod",
+    #root_path="/prod",
     docs_url="/accounts/docs",
     openapi_url="/accounts/openapi.json"
 )
